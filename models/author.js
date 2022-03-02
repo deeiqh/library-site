c8f14673f507152182258fb9bf1902aa7eeabb63
Kevin Salazar <deeiqh@gmail.com>
2022-03-01 19:46:27 -0500

    library project

--- models/author.js
+++
@@ -1,8 +0,0 @@
-mongoose = require('mongoose');
-Schema = mongoose.Schema;
-
-const authorSchema = new Schema({
-    name: String,
-});
-
-module.exports = mongoose.model('Author', authorSchema);
--- models/story.js
+++
@@ -1,9 +0,0 @@
-mongoose = require('mongoose');
-Schema = mongoose.Schema;
-
-const storySchema = new Schema({
-    title: String,
-    author: { type: Schema.Types.ObjectID, ref: 'Author'}
-});
-
-module.exports = mongoose.model('Story', storySchema);
