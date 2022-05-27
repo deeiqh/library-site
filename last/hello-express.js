801e7d5f4ffc95ada8ce570a9ac03200f4dfe68d
Kevin Salazar <deeiqh@gmail.com>
2022-05-27 18:00:21 -0500

    index in public directory, express just with post

--- last/hello-express.js
+++ last/hello-express.js
@@ -6,13 +6,6 @@ back.listen(port, () => console.log(`Listening on port ${port}`));
 
 back.use(express.static('public'));
 
-back.get('/', 
-    (req, res) => {
-        console.log(`Get: ${req.url}`);
-        res.sendFile(`${__dirname}/index.html`);
-    }
-);
-
 back.post('/',
     (req, res) => {
         console.log(`Post: ${req.url}`);
